eb7d98d11d622971b7d90dac8d5535c727fc4328
angel <angel_aguado_k2@hotmail.com>
2023-10-24 18:07:18 -0500

    porafolio

--- script.js
+++ script.js
@@ -21,16 +21,27 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
+        habilidades[0].classList.add("htmlcss");
         habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("photoshop");
         habilidades[3].classList.add("wordpress");
         habilidades[4].classList.add("drupal");
         habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
+        habilidades[6].classList.add("proyect");
+        habilidades[7].classList.add("proyect");
         habilidades[8].classList.add("dedicacion");
         habilidades[9].classList.add("proyect");
+
+        habilidades[10].classList.add("comunicacion");
+        habilidades[11].classList.add("proyect");
+        habilidades[12].classList.add("proyect");
+        habilidades[13].classList.add("proyect");
+        habilidades[14].classList.add("dedicacion");
+        habilidades[15].classList.add("proyect");
+        habilidades[16].classList.add("dedicacion");
+        habilidades[17].classList.add("proyect");
+        habilidades[18].classList.add("dedicacion");
+       
     }
 }
 
